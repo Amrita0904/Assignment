c86fe37d47b8b90c45854ee5ca92de2104aa7e1f
Amrita0904 <amrita05370464@gmail.com>
2025-07-17 23:17:39 +0530

    Add files via upload

---
+++ AMRITA(PROJECT)/task8(17.07.2025)/bgbutton.js
@@ -0,0 +1,35 @@
+ function Arial(){
+document.getElementById("img2").src="https://i.pinimg.com/736x/2d/f1/dd/2df1dd6ea41535807e7ade808212f974.jpg";
+}
+ function Moana(){
+document.getElementById("img2").src="https://i.pinimg.com/736x/9e/f5/66/9ef5662081cfb7717e782a7081dd8bcc.jpg";
+}
+ function Jasmine(){
+document.getElementById("img2").src="https://i.pinimg.com/736x/8e/6b/74/8e6b742a847d99b99b642ec1ae8aa009.jpg";
+}
+ function Enzo(){
+document.getElementById("img2").src="https://i.pinimg.com/1200x/39/b6/07/39b6077421d50dfb06b81c93ac9ff7a7.jpg";
+}
+ function restart(){
+document.getElementById("img2").src="https://i.pinimg.com/736x/69/ca/8e/69ca8e74b45b717b21d0a32e462ba265.jpg";
+}
+
+
+
+ function pastel(){
+document.getElementById("img1").src="https://i.pinimg.com/1200x/65/05/85/65058502b57e45e1c07b7cfecdc0e752.jpg";
+
+
+}
+ function kawaii(){
+document.getElementById("img1").src="https://i.pinimg.com/1200x/33/09/af/3309af3a1e2af328c6f1fcc11f9284eb.jpg";
+}
+ function Sakura(){
+document.getElementById("img1").src="https://i.pinimg.com/736x/20/f2/30/20f230e642bc95c59ae16c8eb524ff02.jpg";
+}
+ function Strawberry(){
+document.getElementById("img1").src="https://i.pinimg.com/736x/43/8e/f6/438ef60f4b0a162ecf35f468213fe547.jpg";
+}
+ function reset(){
+document.getElementById("img1").src="https://i.pinimg.com/736x/69/ca/8e/69ca8e74b45b717b21d0a32e462ba265.jpg";
+}
